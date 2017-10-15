8766918e4a836a92cc848faf2a79fdf911191464
Cade Ritter <ritter.cade@gmail.com>
2017-10-14 21:59:17 -0500

    add comments

--- src/controllers/ride-controller.js
+++ src/controllers/ride-controller.js
@@ -11,6 +11,10 @@ router.use(bodyParser.json());
  * Returns all rides.
  */
 router.get('/', function (request, response) {
+   //'find' returns all objects matching the given query - all objects match the empty query "{}"
+   // Most db operations take a function as their second argument, which is called after the query completes. This
+   // function executes after the operation finishes - if there's an error, the first argument (err) is true. If not,
+   // the second argument (rides) contains our results.
    Ride.find({}, function (err, rides) {
        if (err) {
            return response.status(500); // db error (500 internal server error)
--- src/models/ride.js
+++ src/models/ride.js
@@ -1,5 +1,8 @@
 var mongoose = require('mongoose');
 
+/**
+ * Declare our schema for a Ride.
+ */
 var RideSchema = new mongoose.Schema({
     title: String,
     description: String,
@@ -12,10 +15,35 @@ var RideSchema = new mongoose.Schema({
     arrival_location: { type: 'Point', coordinates: [Number]}, // [long, lat]
     spots: Number,
     vehicle_type: String,
+
+    /* for owner and rider, we need to tell MongoDB to reference the User model here. */
     owner: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
     riders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }]
 });
 
+/**
+ * The owner and riders fields only contain IDs that reference the objects, not the actual obhect. Before we send our
+ * Ride object, we need to 'populate' these fields with their actual objects.
+ * This is called 'middleware.'
+ *
+ * @param next is a function passed in by mongoose that we need to call after we do our population.
+ */
+var autoPopulate = function (next) {
+    this.populate('owner');
+    this.populate('riders');
+    next();
+};
+
+/**
+ * Plug in our middleware we defined above to run before the specified operations.
+ */
+RideSchema.pre('find', autoPopulate);
+RideSchema.pre('findOne', autoPopulate);
+RideSchema.pre('save', autoPopulate);
+
+/**
+ * Declare the model.
+ */
 mongoose.model('Ride', RideSchema);
 
 module.exports = mongoose.model('Ride');
