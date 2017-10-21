f9c6c7567595b2ddf6aa40da986dd1d4946a026e
Miles Olson <mpolson64@gmail.com>
2017-10-21 17:00:00 -0500

    Broke out testing db from production db

--- src/config.js
+++ src/config.js
@@ -1,4 +1,13 @@
-module.exports = {
-    'secret': 'TEST_SECRET',
-    'db_uri': 'mongodb://localhost/Carpool'
-};
+if(process.env.NODE_ENV == 'test') {
+    module.exports = {
+        'secret': 'TEST_SECRET',
+        'db_uri': 'mongodb://localhost/Carpool'
+    };
+}
+
+else {
+    module.exports = {
+        'secret': 'TEST_SECRET',
+        'db_uri': 'mongodb://localhost/Carpool'
+    };
+}
