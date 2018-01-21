b45f5cc5a1c5c171c19578cf2cff85c40bce2139
Miles Olson <mpolson64@gmail.com>
2018-01-21 15:17:24 -0600

    Suppressed eslint no-console in server

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ const app = require('./app');
 const port = process.env.PORT || 3000;
 
 const server = app.listen(port, () => {
+  // eslint-disable-next-line no-console
   console.log(`Carpool backend listening on port ${port}`);
 });
 
