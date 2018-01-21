ad4f9241378bc6e44486bdf05ce31b012fb3a076
Miles Olson <mpolson64@gmail.com>
2018-01-20 21:05:15 -0600

    Added fuzzier searching on time (+- 30 min)

--- src/controllers/search-controller.js
+++ src/controllers/search-controller.js
@@ -2,9 +2,9 @@ var express = require('express');
 var jwt = require('jsonwebtoken');
 var router = express.Router();
 var bodyParser = require('body-parser');
+var moment = require('moment');
 
 var Ride = require('../models/ride');
-var User = require('../models/user');
 var authMiddleWare = require('../middleware/auth-middleware');
 
 router.use(bodyParser.json());
@@ -15,20 +15,62 @@ if(process.env.NODE_ENV !== 'test') {
 }
 
 router.get('/', function(req,res){
+
     const query = new Object();
     if(req.query.departing_from) query.departing_from = req.query.departing_from;
     if(req.query.arriving_at) query.arriving_at = req.query.arriving_at;
-    if(req.query.departure_time) query.departure_time = new Date(req.query.departure_time);
 
-    Ride.find(query, function(err, rides) {
-        if(err) {
-            return res.status(500);
-        }
-        if(!rides) {
-            return res.status(404);
-        }
-        res.status(200).send(rides);
-    });
+    if(req.query.departure_time) {
+        let allRides = [];
+        query.departure_time = moment(req.query.departure_time).toDate();
+
+        Ride.find(query, function(err, rides) {
+            if(err) {
+                return res.status(500);
+            }
+            if(!rides) {
+                return res.status(404);
+            }
+            console.log(rides);
+            allRides = allRides.concat(rides);
+            console.log(allRides);
+
+            query.departure_time = moment(req.query.departure_time).add(30, 'm').toDate();
+
+            Ride.find(query, function(err, rides) {
+                if(err) {
+                    return res.status(500);
+                }
+                if(!rides) {
+                    return res.status(404);
+                }
+                allRides = allRides.concat(rides);
+                query.departure_time = moment(req.query.departure_time).subtract(30, 'm').toDate();
+
+                Ride.find(query, function(err, rides) {
+                    if(err) {
+                        return res.status(500);
+                    }
+                    if(!rides) {
+                        return res.status(404);
+                    }
+                    allRides = allRides.concat(rides);
+                    res.status(201).send(allRides);
+                });
+            });
+        });
+    }
+    else {
+        Ride.find(query, function (err, rides) {
+            if (err) {
+                return res.status(500);
+            }
+            if (!rides) {
+                return res.status(404);
+            }
+            res.status(200).send(rides);
+        });
+    }
 });
 
 module.exports = router;
