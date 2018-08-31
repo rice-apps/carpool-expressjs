94127072fccf4daf2630983e2c1e95083ebf080a
dikshagupta414 <31869978+dikshagupta414@users.noreply.github.com>
2018-08-31 16:37:09 -0500

    Testing

--- src/config.js
+++ src/config.js
@@ -10,9 +10,21 @@ if (process.env.NODE_ENV === 'testaaa') {
 } else {
   module.exports = {
     secret: 'TEST_SECRET',
-    db_uri: 'mongodb://riceapps:r1ce4pps$wag1@carpool-dev-shard-00-00-k86la.mongodb.net:27017,carpool-dev-shard-00-01-k86la.mongodb.net:27017,carpool-dev-shard-00-02-k86la.mongodb.net:27017/test?ssl=true&replicaSet=carpool-dev-shard-0&authSource=admin',
+    db_uri: 'mongodb://carpool2018:carpool2018@ds149353.mlab.com:49353/carpool_dev',
     CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
     thisServiceURL: 'http://localhost:4200/auth',
     frontendURL: 'http://localhost:4200',
   };
+
+    // module.exports = {
+    //     secret: 'TEST_SECRET',
+    //     db_uri: 'mongodb://riceapps:r1ce4pps$wag1@carpool-dev-shard-00-00-k86la.mongodb.net:27017,carpool-dev-shard-00-01-k86la.mongodb.net:27017,carpool-dev-shard-00-02-k86la.mongodb.net:27017/test?ssl=true&replicaSet=carpool-dev-shard-0&authSource=admin',
+    //     CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
+    //     thisServiceURL: 'http://localhost:4200/auth',
+    //     frontendURL: 'http://localhost:4200',
+    // };
 }
+// db_uri: 'mongodb://riceapps:r1ce4pps$wag1@carpool-dev-shard-00-00-k86la.mongodb.net:27017,carpool-dev-shard-00-01-k86la.mongodb.net:27017,carpool-dev-shard-00-02-k86la.mongodb.net:27017/test?ssl=true&replicaSet=carpool-dev-shard-0&authSource=admin',
+// db_uri: 'mongodb://carpool2018:carpool2018@ds149353.mlab.com:49353/carpool_dev',
+// db_uri: 'mongodb://riceapps:r1ce4pps$wag1@carpool-dev-shard-00-00-k86la.mongodb.net:27017,carpool-dev-shard-00-01-k86la.mongodb.net:27017,carpool-dev-shard-00-02-k86la.mongodb.net:27017/test?ssl=true&replicaSet=carpool-dev-shard-0&authSource=admin',
+// db_uri: 'mongodb://carpool2018:carpool2018@ds149353.mlab.com:49353/carpool_dev',
