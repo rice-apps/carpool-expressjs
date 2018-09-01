4170862251ad851ae8784480e39ece08e0184706
myra <myraramdenbourg@gmail.com>
2018-08-31 20:27:59 -0500

    remove auth search and ride controller

--- src/config.js
+++ src/config.js
@@ -16,5 +16,12 @@ if (process.env.NODE_ENV === 'testaaa') {
     thisServiceURL: 'http://localhost:4200/auth',
     frontendURL: 'http://localhost:4200',
   };
-}
 
+    // module.exports = {
+    //     secret: 'TEST_SECRET',
+    //     db_uri: 'mongodb://riceapps:r1ce4pps$wag1@carpool-dev-shard-00-00-k86la.mongodb.net:27017,carpool-dev-shard-00-01-k86la.mongodb.net:27017,carpool-dev-shard-00-02-k86la.mongodb.net:27017/test?ssl=true&replicaSet=carpool-dev-shard-0&authSource=admin',
+    //     CASValidateURL: 'https://idp.rice.edu/idp/profile/cas/serviceValidate',
+    //     thisServiceURL: 'http://localhost:4200/auth',
+    //     frontendURL: 'http://localhost:4200',
+    // };
+}
