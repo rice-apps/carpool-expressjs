acaa381a5c87731d1bc1077580ba944471f9ea3a
Angela Hwang <hwangangela99@hotmail.com>
2018-11-03 14:42:53 -0500

    backend returns rides sorted by ascending date by default

--- src/controllers/search-controller.js
+++ src/controllers/search-controller.js
@@ -22,22 +22,28 @@ router.get('/', (req, res) => {
   if (req.query.departing_from) query.departing_from = req.query.departing_from;
   if (req.query.arriving_at) query.arriving_at = req.query.arriving_at;
 
-  Ride.find(query, (err, rides) => {
+  // If departure_time is provided, provide rides occurring within 30 minutes.
+  if (req.query.departure_time) {
+    const target = new Date(req.query.departure_time).getTime();
+    query.departing_datetime = { $gte: target - 1800000, $lte: target + 1800000};
+  }
+
+  // else, only filter out past rides.
+  else {
+    const currentTime = new Date().getTime();
+    query.departing_datetime = { $gte: currentTime};
+  }
+
+  var finalQuery = Ride.find(query).sort({departing_datetime : 1});
+  finalQuery.exec( (err, rides) => {
     console.log("All rides: " + rides);
 
     if (err) {
       return res.status(500);
     }
-    // If a departure_time is provided, only return rides within 30 minutes of it.
-    if (req.query.departure_time) {
-      const target = new Date(req.query.departure_time).getTime();
 
-      return res.status(200).send(rides.filter(ride => inTimeRange(new Date(ride.departing_datetime).getTime(), target, 1800000))); // 30 min tol
-    }
+    return res.status(200).send(rides);
 
-    // Otherwise, only filter out rides that are in the future.
-    const currentTime = Date.now();
-    return res.status(200).send(rides.filter(ride => inFuture(new Date(ride.departing_datetime).getTime(), currentTime)));
   });
 });
 
